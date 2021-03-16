20aab5fd90e6f642f224cb9265834f744e342cb7
davide <davidecorsinimilano@gmail.com>
2021-03-16 18:13:49 +0100

    Layot shop complete!

--- resources/views/shop.blade.php
+++ resources/views/shop.blade.php
@@ -2,17 +2,14 @@
 
 
 @section('content')
-    <div style="max-width: 980px; border: 1px solid black;"class="container d-flex flex-wrap">
+    <div   class="container shop-cont d-flex flex-wrap">
         @foreach ($dresses as $item)
 
-            <div style="height: 500px; width: calc(100%/3); border: 1px solid rgba(211, 211, 211, 0.5); height: 200px;" class="box">
-                <div class="box-poster" >
-                    <img style="width: 100%; height: 100%;" src="{{$item['poster']}}" alt="">
-                </div>
-                <p>{{$item->name}}</p>
-                <p>{{$item->brand}}</p>
-                <p>{{$item->type}}</p>
-                <p>{{$item->price}}€</p>
+            <div class="box d-flex-c justify-content-center">
+                <img class="poster" src="{{$item['poster']}}" alt="">
+                <h3>{{$item->brand}}</h3>
+                <p>{{$item->name}} - {{$item->type}}</p>
+                <p class="price">{{$item->price}}€</p>
 
             </div>
 
