169492d96b099ee5ce1426605c3a7b9bc9619776
davide <davidecorsinimilano@gmail.com>
2021-03-16 16:52:59 +0100

    add Controller, add Model, add Comunication with Database Shop

--- app/Http/Controllers/MyController.php
+++ app/Http/Controllers/MyController.php
@@ -3,14 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Dress;
+
 
 class MyController extends Controller
 {
     public function home(){
-        return view('home');
+        $data_shop = Dress::all();
+        $data = [
+            'dresses' => $data_shop
+        ];
+        
+        return view('home', $data);
     }
 
     public function contact(){
         return view('contatti');
     }
+
+    
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,24 @@
 @extends('layouts.app')
 
 @section('content')
+    <h1 style="text-align: center;">WELCOME TO THE SHOP</h1>
 
-    home page ciao sono la homepage
+    <div style="max-width: 980px; border: 1px solid black;"class="container d-flex flex-wrap">
+        @foreach ($dresses as $item)
+
+            <div class="box">
+                <p>{{$item['name']}}</p>
+                <p>{{$item['brand']}}</p>
+                <p>{{$item['type']}}</p>
+                <p>{{$item['price']}}€</p>
+
+            </div>
+    
+        
+            
+        @endforeach
+
+    </div>
+
     
 @endsection
