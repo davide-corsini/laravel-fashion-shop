b4db3f5b4fd7bc751c2183e7cea274ed78a8a92d
davide <davidecorsinimilano@gmail.com>
2021-03-16 17:14:34 +0100

    working on style

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,8 @@
     <div style="max-width: 980px; border: 1px solid black;"class="container d-flex flex-wrap">
         @foreach ($dresses as $item)
 
-            <div class="box">
+            <div style="width: calc(100%/3); border: 1px solid rgba(211, 211, 211, 0.5); height: 200px;" class="box">
+                <img style="width: 80%; height: 80%;" src="{{$item['poster']}}" alt="">
                 <p>{{$item['name']}}</p>
                 <p>{{$item['brand']}}</p>
                 <p>{{$item['type']}}</p>
